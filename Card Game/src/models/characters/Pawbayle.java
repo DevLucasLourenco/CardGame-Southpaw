2f7f3198723e0fbd7c528944e575a0a18ffbcad1
Lucas Lourenço <dev.lucaslourenco@gmail.com>
2024-07-03 16:56:56 -0300

    att

--- Card Game/src/models/characters/Pawbayle.java
+++ Card Game/src/models/characters/Pawbayle.java
@@ -5,15 +5,17 @@ import models.contracts.hasPower;
 
 public class Pawbayle extends PawCard implements hasPower{
 
+    public Pawbayle(models.users.User user){
+        super(user);
+    }
+
     @Override
     public void usePower() {
-        // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'usePower'");
     }
 
     @Override
     public void setCardDetails() {
-        // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'setCardDetails'");
     }
 
--- Card Game/src/models/characters/pawbase/managePaws.java
+++ Card Game/src/models/characters/pawbase/managePaws.java
@@ -1,12 +1,10 @@
 package models.characters.pawbase;
 
 import java.util.List;
-import models.characters.Pawarrior;
-import models.characters.Pawclown;
-import models.characters.Pawskeleton;
+import models.characters.*;
 
 public class managePaws {
-    private static List<Class<? extends PawCard>> PawsAvailableForUsage = List.of(
+    private static final List<Class<? extends PawCard>> PawsAvailableForUsage = List.of(
         Pawarrior.class,
         Pawskeleton.class,
         Pawclown.class
