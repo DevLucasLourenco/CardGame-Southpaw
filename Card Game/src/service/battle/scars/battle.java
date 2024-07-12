fa065239c494f6902abcbee562c77500f759582b
Lucas Lourenço <dev.lucaslourenco@gmail.com>
2024-07-12 15:44:06 -0300

    S

--- Card Game/src/service/battle/scars/battle.java
+++ Card Game/src/service/battle/scars/battle.java
@@ -11,37 +11,37 @@ public class battle {
         Scanner scanner = new Scanner(System.in);
 
         // Receive Names
-        //========================================
+        // ========================================
         System.out.println("Set your infos.\n---------------------------------\n");
         System.out.println("About the FIRST player, what's your name?");
         String firstName = scanner.nextLine().toUpperCase();
-        
+
         System.out.println("About the SECOND player, what's your name?");
         String secondName = scanner.nextLine().toUpperCase();
-        //========================================
+        // ========================================
 
         // Settings Players
-        //========================================
+        // ========================================
         User user1 = new User(firstName);
         User user2 = new User(secondName);
-        //========================================
-        
+        // ========================================
+
         // Primordial Settings and Instanciations
-        //========================================
+        // ========================================
         PartyDetection party = new PartyDetection();
         eventDeal event = new eventDeal();
-        
+
         party.insertUsers(user1, user2);
         event.insertUsers(user1, user2);
-        //========================================
+        // ========================================
 
         // Menu
-        //========================================
+        // ========================================
         event.FirstMenu();
 
-        //dps
+        // dps
         event.ChooseMenu();
-        //========================================
+        // ========================================
 
     }
 }
